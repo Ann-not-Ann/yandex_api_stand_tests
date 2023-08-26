299fe101369b0e9af0ce2736cff3eecd62343069
Ann-not-Ann <142057567+Ann-not-Ann@users.noreply.github.com>
2023-08-26 15:01:04 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://5b4639b5-45ad-4115-9fe7-ef1747e086e4.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
